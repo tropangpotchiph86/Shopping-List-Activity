9fc95bdd05de2862fdb1e46a176f618fef237e0c
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-05 13:03:05 +0800

    08lseditmode

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearBtn = document.getElementById('clear');
 const itemFilter = document.getElementById('filter');
+const formBtn = itemForm.querySelector('button');
+
+let isEditMode = false;
 
 //Functions
 
@@ -67,7 +70,22 @@ function createIcon(classes) {
 function onClickItem(e) {
   if (e.target.parentElement.classList.contains('remove-item')) {
     removeItem(e.target.parentElement.parentElement);
+  } else {
+    setItemtoEdit(e.target);
   }
+}
+
+function setItemtoEdit(item) {
+  isEditMode = true;
+
+  itemList
+    .querySelectorAll('li')
+    .forEach((i) => i.classList.remove('edit-mode'));
+
+  item.classList.add('edit-mode');
+  formBtn.innerHTML = '<i class="fa-solid fa-pen"></i>  Update Item';
+  itemInput.value = item.textContent;
+  formBtn.style.backgroundColor = '#228B22';
 }
 
 function removeItem(item) {
