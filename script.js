8fc6fdd3b144968f0d7653babd1735a81049521b
tropangpotchiph86 <tropangpotchiph@gmail.com>
2023-10-05 09:41:44 +0800

    Codes for adding items on the list thru the Add Item Button

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-//########################################
-//########################################
-// localStorage.setItem('name', 'Ryan');
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
 
-// console.log(localStorage.getItem('name'));
+//Functions
+function addItem(e) {
+  e.preventDefault();
+  const newItem = itemInput.value;
 
-// // localStorage.removeItem('name');
+  //Validate Input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
 
-// localStorage.clear();
+  //Create list item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+
+  itemList.appendChild(li);
+
+  itemInput.value = '';
+}
+
+function createButton(classes) {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+}
+
+function createIcon(classes) {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+}
+
+//Event Listeners
+itemForm.addEventListener('submit', addItem);
